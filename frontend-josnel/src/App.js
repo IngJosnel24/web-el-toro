ae8280d9f85ef8aa3cd4327cbb5b70508ecff8bf
Oneyker21 <oneygaleano21@gmail.com>
2023-10-17 23:36:38 -0600

    eliminado teachers.

--- frontend-josnel/src/App.js
+++ frontend-josnel/src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Teacher from './pages/Teacher';
-import TeacherList from './pages/TeacherList';
 import Productos from './pages/Productos';
 import Producto from './pages/ProductoList';
 
@@ -13,8 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/teacher" element={<Teacher />} />
-        <Route path="/teacherList" element={<TeacherList />} />
         <Route path="/producto" element={<Productos />} />
         <Route path="/productolist" element={<Producto />} />
       </Routes>
